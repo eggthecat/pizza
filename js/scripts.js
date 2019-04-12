0c5e91670992a4eced6324261c7097e8b8ad7536
Dominic Montelongo <montelongodominic@gmail.com>
2019-04-12 13:17:08 -0700

    Add css, adjust html, and adjust js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //business logic for Customer
 
 function Customer() {
-this.contact = []
-this.currentId = -1
+this.pie = []
+this.pieId = -1
 }
-Customer.prototype.addOrder = function(order){
-  order.id = this.assignId
-  this.order.push(contact);
+Customer.prototype.addOrder = function(pie){
+  pie.id = this.assignId
+  this.pie.push(contact);
 }
 
 //business logic for
