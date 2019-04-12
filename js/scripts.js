62e55854cfd4e3cb7efbc5c7ded005505e6ccfa9
Dominic Montelongo <montelongodominic@gmail.com>
2019-04-12 13:01:27 -0700

    Add background img

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
-//business logic for Pizza Order
+//business logic for Customer
 
-function customer() {
+function Customer() {
 this.contact = []
 this.currentId = -1
 }
-pizzaOrder.prototype.addOrder = function(order){
-
+Customer.prototype.addOrder = function(order){
+  order.id = this.assignId
+  this.order.push(contact);
 }
 
 //business logic for
