d9d56140d65295bcc6117969cf87e37c30587a83
Dominic Montelongo <montelongodominic@gmail.com>
2019-04-12 14:19:08 -0700

    Play around with js

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,42 @@ function Customer() {
 this.pie = []
 this.pieId = -1
 }
+
 Customer.prototype.addOrder = function(pie){
   pie.id = this.assignId
   this.pie.push(contact);
 }
 
-//business logic for
+Customer.prototype.assignId = function(
+  this,currentId += 1;
+  return this.currentId;
+)
+//business logic for Pizza
+function Pie(quantity, size, toppings, sauce) {
+this.size = [];
+this.toppings = [];
+this.sauce = [];
+this.price = [];
+}
+Pie.prototype.addSize = function(size) {
+  this.size.push(sizeSelected);
+}
+
+Pie.prototype.addToppings = function(toppings) {
+  this.toppings.push(toppingsSelected);
+}
+Pie.prototype.addSauce = function(sauce) {
+  this.sauce.push(sauceSelected);
+}
+
+Pizza.prototype.getSauce = function(sauce)
+  if(this.sauce === "sauceless") {
+    this.sauce
+  }
+Pie.prototype.addPrice = function(price) {
+  this.price.push(priceSelected);
+}
+
+
+//user interfact logic
+$(document).ready(function() {})
